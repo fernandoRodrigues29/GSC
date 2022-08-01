5b7667a4943173d26d560f941ed557d61f390f0a
fernandoRodrigues29 <fernando1587@gmail.com>
2022-08-01 09:38:41 -0300

    tentativa 2

---
+++ js/about.a1677f98.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkapp"]=self["webpackChunkapp"]||[]).push([[443],{381:function(n,a,u){u.r(a),u.d(a,{default:function(){return f}});var t=u(821);const e={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function c(n,a){return(0,t.wg)(),(0,t.iD)("div",e,s)}var i=u(744);const p={},o=(0,i.Z)(p,[["render",c]]);var f=o}}]);
+//# sourceMappingURL=about.a1677f98.js.map
